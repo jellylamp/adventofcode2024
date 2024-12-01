57d65736b3ac26f08555806f5c10e1ad5c64bae7
Caroline Galecki <Caroline@macbook-air-2.lan>
2024-12-01 00:35:19 -0500

    Refactor and day 1 part b

--- day01/day01.go
+++ day01/day01.go
@@ -7,10 +7,12 @@ import (
 	"sort"
 )
 
-func Run(filename string)(int) {
+var firstDigitList []int
+var secondDigitList []int
+
+func parseInput(filename string) {
     input := utils.ReadFileToArrayOfLines(filename)
-	var firstDigitList []int
-	var secondDigitList []int
+
 	for index, value := range input {
         fmt.Printf("Index: %d, Value: %s\n", index, value)
 		digitsArr := strings.Split(value, "   ")
@@ -19,6 +21,10 @@ func Run(filename string)(int) {
 	    firstDigitList = append(firstDigitList, firstDigit)
 		secondDigitList = append(secondDigitList, secondDigit)
     }
+}
+
+func PartA(filename string)(int) {
+	parseInput(filename)
 
 	// sort both lists by smallest
     sort.Ints(firstDigitList)
@@ -31,3 +37,27 @@ func Run(filename string)(int) {
 	}
 	return totalDistanceCalc
 }
+
+func PartB(filename string)(int) {
+	parseInput(filename)
+
+	// sort both lists by smallest
+    sort.Ints(firstDigitList)
+	sort.Ints(secondDigitList)
+
+	countMap := make(map[int]int)
+
+	var totalSimilarityScore = 0
+
+	// add second list to a map
+	for _, num := range secondDigitList {
+		countMap[num]++
+	}
+
+	for index := range firstDigitList {
+		similarity := firstDigitList[index] * countMap[firstDigitList[index]]
+		totalSimilarityScore += similarity
+	}
+	return totalSimilarityScore
+}
+
--- day01/day01_test.go
+++ day01/day01_test.go
@@ -3,7 +3,7 @@ package day01
 import "testing"
 
 func TestSamplePartA(t *testing.T) {
-    output := Run("./samples/sample.txt")
+	output := PartA("./samples/sample.txt")
 	expected := 11
 	if output != expected {
 		t.Errorf("sample part a = %d; expected %d", output, expected)
@@ -11,10 +11,25 @@ func TestSamplePartA(t *testing.T) {
 }
 
 func TestPartA(t *testing.T) {
-    output := Run("./samples/parta.txt")
+	output := PartA("./samples/parta.txt")
 	expected := 1506483
 	if output != expected {
 		t.Errorf("sample part a = %d; expected %d", output, expected)
 	}
 }
 
+func TestSamplePartB(t *testing.T) {
+	output := PartB("./samples/sample.txt")
+	expected := 31
+	if output != expected {
+		t.Errorf("sample part a = %d; expected %d", output, expected)
+	}
+}
+
+func TestPartB(t *testing.T) {
+	output := PartB("./samples/parta.txt")
+	expected := 23126924
+	if output != expected {
+		t.Errorf("sample part a = %d; expected %d", output, expected)
+	}
+}
