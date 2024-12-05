b654baaa5b24f8475331831b4ff40cf917fc2699
Caroline Galecki <Caroline@macbook-air-2.lan>
2024-12-05 15:32:09 -0500

    day 5 part 2

--- day05/day05.go
+++ day05/day05.go
@@ -2,17 +2,15 @@ package day05
 
 import (
 	"adventofcode2024/utils"
+	"fmt"
 	"strings"
 )
 
 func PartA(filename string) int {
 	input := utils.ReadFileToArrayOfLines(filename)
 	orderMap := make(map[string][]string)
-	updateList := []string{}
-
 	inOrderingRules := true
 	middleSums := 0
-	correctRules := [][]string{}
 
 	for _, line := range input {
 		// loop through input and add to ordering rules
@@ -29,10 +27,8 @@ func PartA(filename string) int {
 			orderMap[beforeNum] = append(orderMap[beforeNum], afterNum)
 		} else {
 			// put update instructions in its own list; in case we need it
-			updateList = append(updateList, line)
 			splitArr := strings.Split(line, ",")
 			if isInstructionCorrect(splitArr, orderMap) {
-				correctRules = append(correctRules, splitArr)
 				middleSums += findMiddleAndConvertToNum(splitArr)
 			}
 		}
@@ -41,12 +37,76 @@ func PartA(filename string) int {
 	return middleSums
 }
 
+func PartB(filename string) int {
+	input := utils.ReadFileToArrayOfLines(filename)
+	orderMap := make(map[string][]string)
+	inOrderingRules := true
+	middleSums := 0
+
+	for _, line := range input {
+		// loop through input and add to ordering rules
+		// split the line before | after
+		// add to map, key is the number to look up
+		if line == "" {
+			inOrderingRules = false
+			continue
+		}
+		if inOrderingRules {
+			splitList := strings.Split(line, "|")
+			beforeNum := splitList[0]
+			afterNum := splitList[1]
+			orderMap[beforeNum] = append(orderMap[beforeNum], afterNum)
+		} else {
+			// put update instructions in its own list; in case we need it
+			splitArr := strings.Split(line, ",")
+			if !isInstructionCorrect(splitArr, orderMap) {
+				reorderedArr := reorderInstructions(splitArr, orderMap)
+				if isInstructionCorrect(reorderedArr, orderMap) {
+					middleSums += findMiddleAndConvertToNum(reorderedArr)
+				} else {
+					fmt.Println("ruh roh")
+				}
+			}
+		}
+	}
+
+	return middleSums
+}
+
+func reorderInstructions(splitArr []string, orderMap map[string][]string) []string {
+	// ok so we have a map of correct instructions
+
+	updatedOrderList := make([]string, len(splitArr))
+	copy(updatedOrderList, splitArr)
+
+	// loop over list and make a secondary list running
+	for _, runningSplitArrValue := range splitArr {
+		// take first item, check its correctness. if wrong, move index
+		orderRules := orderMap[runningSplitArrValue]
+		for _, orderRule := range orderRules {
+			// if ordered rule is in the splitArr, make sure its after value index
+			orderedRuleIndexInSplitArr := utils.IndexOf(updatedOrderList, orderRule)
+			// get the latest order, in case it moved
+			runningSplitIndexInOrderList := utils.IndexOf(updatedOrderList, runningSplitArrValue)
+
+			// if orderedRuleIndexInSplitArr is -1, then this rule isnt in split arr
+			if orderedRuleIndexInSplitArr != -1 && orderedRuleIndexInSplitArr <= runningSplitIndexInOrderList {
+
+				// Remove the element from the "from" index
+				updatedOrderList = append(updatedOrderList[:runningSplitIndexInOrderList], updatedOrderList[runningSplitIndexInOrderList+1:]...)
+
+				orderedRuleIndexInOrderList := utils.IndexOf(updatedOrderList, orderRule)
+				// Insert the element at the correct position
+				updatedOrderList = append(updatedOrderList[:orderedRuleIndexInOrderList], append([]string{runningSplitArrValue}, updatedOrderList[orderedRuleIndexInOrderList:]...)...)
+
+			}
+		}
+	}
+	return updatedOrderList
+}
+
 func isInstructionCorrect(splitArr []string, orderMap map[string][]string) bool {
 	// loop over splitArr, grabbing each character and making sure that the items behind it are allowed
-
-	// The fifth update, 61,13,29, is also not in the correct order, since it breaks the rule 29|13.
-	// The last update, 97,13,75,29,47, is not in the correct order due to breaking several rules.
-
 	for valueIndex, value := range splitArr {
 		valueRules := orderMap[value]
 		for _, valueRule := range valueRules {
--- day05/day05_test.go
+++ day05/day05_test.go
@@ -18,18 +18,18 @@ func TestPartA(t *testing.T) {
 	}
 }
 
-// func TestSamplePartB(t *testing.T) {
-// 	output := PartB("./samples/partbsample.txt")
-// 	var expected int64 = 48
-// 	if output != expected {
-// 		t.Errorf("sample part b = %d; expected %d", output, expected)
-// 	}
-// }
+func TestSamplePartB(t *testing.T) {
+	output := PartB("./samples/sample.txt")
+	expected := 123
+	if output != expected {
+		t.Errorf("sample part b = %d; expected %d", output, expected)
+	}
+}
 
-// func TestPartB(t *testing.T) {
-// 	output := PartB("./samples/parta.txt")
-// 	var expected int64 = 88811886
-// 	if output != expected {
-// 		t.Errorf("sample part a = %d; expected %d", output, expected)
-// 	}
-// }
+func TestPartB(t *testing.T) {
+	output := PartB("./samples/input.txt")
+	expected := 4230 // too low
+	if output != expected {
+		t.Errorf("sample part b = %d; expected %d", output, expected)
+	}
+}
