8bad6bcf5a9455064dc2f01507065665e33ef5dc
Caroline Galecki <Caroline@macbook-air-2.lan>
2024-12-16 22:38:28 -0500

    day 12 part b wip

--- day12/day12.go
+++ day12/day12.go
@@ -98,9 +98,11 @@ func PartB(filename string) int {
 				continue
 			}
 
+			// b one too many perimeter
+
 			// recursively search for complete areas; mark locations on the visited map.
 			// if visited move on; once we have found the full maps; grab the area and perimeter
-			findFullAreaPartB(grid, character, rowIndex, colIndex, utils.N, utils.E)
+			findFullAreaPartB(grid, character, rowIndex, colIndex, utils.N, -1, -1, rowIndex, colIndex)
 			runningWoodPrice += area * perimeter
 			area = 0
 			perimeter = 0
@@ -110,7 +112,16 @@ func PartB(filename string) int {
 	return runningWoodPrice
 }
 
-func findFullAreaPartB(grid [][]string, letter string, currentRow int, currentCol int, previousDirection utils.Direction, currentDirection utils.Direction) {
+type Fence struct {
+	fenceDirection utils.Direction
+	row            int
+	col            int
+	letter         string
+}
+
+var fenceMap = make(map[Fence]bool)
+
+func findFullAreaPartB(grid [][]string, letter string, currentRow int, currentCol int, currentDirection utils.Direction, previousRow int, previousCol int, startingRow int, startingCol int) {
 	// check visited!
 	location := utils.Position{Row: currentRow, Column: currentCol}
 	_, exists := visitedMap[location]
@@ -124,54 +135,93 @@ func findFullAreaPartB(grid [][]string, letter string, currentRow int, currentCo
 
 	// check NESW if matching, keep going; else add to perimeter
 	checkNorthValue, northRow, northCol := utils.GetValidStringByVector(grid, currentRow, currentCol, utils.N)
-	if checkNorthValue == letter {
-		if previousDirection != utils.S {
-			// keep going!
-			findFullAreaPartB(grid, letter, northRow, northCol, currentDirection, utils.N)
+	if checkNorthValue != letter {
+		// when I would normally add a perimeter; I should instead first check if I already have one in the previous location and vector
+		_, previousFenceExists := fenceMap[Fence{fenceDirection: utils.N, row: previousRow, col: previousCol, letter: letter}]
+		loopedBackToStarter := false
+		if northRow == startingRow && northCol == startingCol {
+			loopedBackToStarter = true
 		}
-	} else {
-		// this doesn't match but we gain a perimeter; but don't want to add if we already have; OR add if we are starting to move back
-		if previousDirection != currentDirection || currentDirection == utils.N && previousDirection == utils.N {
+
+		if !previousFenceExists && !loopedBackToStarter {
 			perimeter++
 		}
+		fenceMap[Fence{fenceDirection: utils.N, row: currentRow, col: currentCol, letter: letter}] = true
 	}
 
 	checkEastValue, eastRow, eastCol := utils.GetValidStringByVector(grid, currentRow, currentCol, utils.E)
-	if checkEastValue == letter {
-		if previousDirection != utils.W {
-			// keep going!
-			findFullAreaPartB(grid, letter, eastRow, eastCol, currentDirection, utils.E)
+	if checkEastValue != letter {
+		// when I would normally add a perimeter; I should instead first check if I already have one in the previous location and vector
+		_, previousFenceExists := fenceMap[Fence{fenceDirection: utils.E, row: previousRow, col: previousCol, letter: letter}]
+		loopedBackToStarter := false
+		if eastRow == startingRow && eastCol == startingCol {
+			loopedBackToStarter = true
 		}
-	} else {
-		// this doesn't match but we gain a perimeter; but don't want to add if we already have
-		if previousDirection != currentDirection || currentDirection == utils.E && previousDirection == utils.E {
+
+		if !previousFenceExists && !loopedBackToStarter {
 			perimeter++
 		}
+		fenceMap[Fence{fenceDirection: utils.E, row: currentRow, col: currentCol, letter: letter}] = true
 	}
 
 	checkSouthValue, southRow, southCol := utils.GetValidStringByVector(grid, currentRow, currentCol, utils.S)
-	if checkSouthValue == letter {
-		if previousDirection != utils.N {
-			// keep going!
-			findFullAreaPartB(grid, letter, southRow, southCol, currentDirection, utils.S)
+	if checkSouthValue != letter {
+		// when I would normally add a perimeter; I should instead first check if I already have one in the previous location and vector
+		_, previousFenceExists := fenceMap[Fence{fenceDirection: utils.S, row: previousRow, col: previousCol, letter: letter}]
+		loopedBackToStarter := false
+		if southRow == startingRow && southCol == startingCol {
+			loopedBackToStarter = true
 		}
-	} else {
-		// this doesn't match but we gain a perimeter; but don't want to add if we already have
-		if previousDirection != currentDirection || currentDirection == utils.S && previousDirection == utils.S {
+
+		if !previousFenceExists && !loopedBackToStarter {
 			perimeter++
 		}
+		fenceMap[Fence{fenceDirection: utils.S, row: currentRow, col: currentCol, letter: letter}] = true
 	}
 
 	checkWestValue, westRow, westCol := utils.GetValidStringByVector(grid, currentRow, currentCol, utils.W)
-	if checkWestValue == letter {
-		if previousDirection != utils.E {
-			// keep going!
-			findFullAreaPartB(grid, letter, westRow, westCol, currentDirection, utils.W)
+	if checkWestValue != letter {
+		// when I would normally add a perimeter; I should instead first check if I already have one in the previous location and vector
+		_, previousFenceExists := fenceMap[Fence{fenceDirection: utils.W, row: previousRow, col: previousCol, letter: letter}]
+		loopedBackToStarter := false
+		if westRow == startingRow && westCol == startingCol {
+			loopedBackToStarter = true
 		}
-	} else {
-		// this doesn't match but we gain a perimeter; but don't want to add if we already have
-		if previousDirection != currentDirection || currentDirection == utils.W && previousDirection == utils.W {
+		// b is broken here because west row == 2  and west col == -1
+		// compare starting row/column/direction? no of course that will always exist it has to match what WE have
+		if !previousFenceExists && !loopedBackToStarter {
 			perimeter++
 		}
+		fenceMap[Fence{fenceDirection: utils.W, row: currentRow, col: currentCol, letter: letter}] = true
+	}
+
+	/////////////
+	if checkNorthValue == letter {
+		// don't go backwards down the same route
+		// if previousDirection != utils.S {
+		// keep going!
+		findFullAreaPartB(grid, letter, northRow, northCol, utils.N, currentRow, currentCol, startingRow, startingCol)
+		// }
+	}
+
+	if checkEastValue == letter {
+		// if previousDirection != utils.W {
+		// keep going!
+		findFullAreaPartB(grid, letter, eastRow, eastCol, utils.E, currentRow, currentCol, startingRow, startingCol)
+		// }
+	}
+
+	if checkSouthValue == letter {
+		// if previousDirection != utils.N {
+		// keep going!
+		findFullAreaPartB(grid, letter, southRow, southCol, utils.S, currentRow, currentCol, startingRow, startingCol)
+		// }
+	}
+
+	if checkWestValue == letter {
+		// if previousDirection != utils.E {
+		// keep going!
+		findFullAreaPartB(grid, letter, westRow, westCol, utils.W, currentRow, currentCol, startingRow, startingCol)
+		// }
 	}
 }
